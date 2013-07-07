e4af2b30c23737c360be9686e62a771a8b0823ef
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-07-07 09:14:55 +0000

    Make it work on Node 0.8.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fileContent = fs.readFileSync('index.html', { encoding: 'utf-8' });
+  var fileContent = fs.readFileSync('index.html').toString('utf-8');
   response.send(fileContent);
 });
 
