0b91889bbb9ab7e36dd737e17f64f045de2dca28
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-07-07 09:09:47 +0000

    Show index.html.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fileContent = fs.readFileSync('index.html', { encoding: 'utf-8' });
+  response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
