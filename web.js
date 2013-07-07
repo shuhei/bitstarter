9276677c64a1f505487a3e43e8ef06901b0010b4
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-07-07 09:18:47 +0000

    Use the lastest node, npm and express.

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
   var fileContent = fs.readFileSync('index.html').toString('utf-8');
@@ -10,5 +11,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.error('Listening on ' + port);
 });
